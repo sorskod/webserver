1bb89fc3f4901fd5255f6c07fd7b255296e70aad
Aleksanandar Babic <salebab@gmail.com>
2019-02-27 13:58:15 +0100

    Fixing CD

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_23c8dfef3c63_key -iv $encrypted_23c8dfef3c63_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
-    gpg --batch --fast-import cd/codesigning.asc
+
+if [[ -z "$encrypted_23c8dfef3c63_key" || -z "$encrypted_23c8dfef3c63_iv" ]]
+  then
+    echo "Variables for OpenSSL are not declared."
+    exit 1;
 fi
+
+openssl aes-256-cbc -K $encrypted_23c8dfef3c63_key -iv $encrypted_23c8dfef3c63_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+gpg --batch --fast-import cd/codesigning.asc
--- cd/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -P sign,build-extras -s cd/maven-settings.xml -DskipTests
-fi
